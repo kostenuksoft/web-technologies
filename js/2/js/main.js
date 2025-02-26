cb677629aef3c405a3d48f0dc278466f912a2590
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2025-02-26 21:22:04 +0200

    Add sequential task showcase source

---
+++ js/2/js/main.js
@@ -0,0 +1,14 @@
+const myObj1 = { age: 18, name: "vlad" };
+const myObj2 = { age: 30, name: "vlad" };
+
+console.log
+(
+   findMinMax([1, 5, 3, 9, 2]),
+   compareObjects(myObj1, myObj2, ["age", "name"]),
+   isInRange(15, 10, 20),
+   toggleBoolean(true),
+   getGradeDescription(50),
+   getGradeDescriptionTernary(75),
+   getSeason(7),
+   getSeasonTernary(3)
+);
