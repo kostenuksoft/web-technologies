8a81d983c73151d16380434bced69168b8fbc191
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2025-02-26 21:27:16 +0200

    Add sequential task showcase source

---
+++ js/3/js/main.js
@@ -0,0 +1,19 @@
+
+console.log("~ TASK [1] -> ",
+    sumFirstTenFibonacci());
+
+console.log("~ TASK [2] -> ",
+    sumPrimes(1000));
+
+console.log("~ TASK [3] -> ",
+    getDayOfWeek(3));
+
+console.log("~ TASK [4] -> ",
+    filterOddLengthStrings(
+        ["vlad", "vladyslav", "javascripty", "vladyslave", "oddstring"]));
+
+console.log("~ TASK [5] -> ",
+    incrementArray([1, 2, 3, 4, 5]));
+
+console.log("~ TASK [6] -> ",
+    isSumOrDifferenceTen(20, 10));
